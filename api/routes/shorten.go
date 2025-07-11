6fdbaf9b3e015562966cc23e1e76abe9501f8402
Ragib Shahriar <ragibshahriar58@gmail.com>
2025-07-11 12:09:11 +0600

    shorten & getUrl

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -7,14 +7,17 @@ import (
 	"time"
 	"url-shortner/api/database"
 	"url-shortner/api/models"
+	"url-shortner/api/utils"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
 )
 
 func ShortenURL(c *gin.Context) {
 	var body models.Request
-	if err := c.ShouldBindJSON(&body); err != nil {
+	if err := c.ShouldBind(&body); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot Parse JSON"})
 		return
 	}
@@ -39,4 +42,65 @@ func ShortenURL(c *gin.Context) {
 			return
 		}
 	}
+
+	if !govalidator.IsURL(body.URL) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid URL"})
+		return
+	}
+
+	if !utils.IsDifferentDomain(body.URL) {
+		c.JSON(http.StatusServiceUnavailable, gin.H{
+			"error": "You can't hack this System :)",
+		})
+		return
+	}
+
+	body.URL = utils.EnsureHTTPPrefix(body.URL)
+
+	var id string
+
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Custom short URL already exists"})
+		return
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to connect to the redis server"})
+		return
+	}
+
+	resp := models.Response{
+		Expiry:          body.Expiry,
+		XRateLimitReset: 30,
+		XRateRemaining:  10,
+		URL:             body.URL,
+		CustomShort:     "",
+	}
+
+	r2.Decr(database.Ctx, c.ClientIP())
+
+	val, _ = r2.Get(database.Ctx, c.ClientIP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.ClientIP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	c.JSON(http.StatusOK, resp)
 }
